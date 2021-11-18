67be1788c52d1fc899fd6d1c904d5682ec439ecf
Nikolas Alexander Damalas <ndamalas@data.cs.purdue.edu>
2021-11-18 16:07:09 -0500

    added key serialization

--- alice.py
+++ alice.py
@@ -5,6 +5,7 @@ from cryptography.hazmat.backends import default_backend
 import time, socket, pickle, os
 
 from encrypt_decrypt import encrypt, decrypt
+from cryptography.hazmat.primitives import serialization
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
@@ -13,11 +14,18 @@ alice_priv = ec.generate_private_key(ec.SECP384R1(), default_backend())
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT)) #Connect to the socket
 
-    s.sendall(alice_priv.public_key()) #Send over alice's public key
+    #serialize key
+    serialKey = alice_priv.public_key().public_bytes(encoding=serialization.Encoding.PEM,format=serialization.PublicFormat.SubjectPublicKeyInfo)
+    s.sendall(serialKey) #Send over alice's public key
     print("Set alice's public key! Receiving bob's public key now")
     bob_public = s.recv(1024)
 
-    alice_shared = alice_priv.exchange(ec.ECDH(), bob_public)
+    #deserialize key
+    loaded_public_key = serialization.load_pem_public_key(bob_public, default_backend())
+
+    print(loaded_public_key)
+
+    alice_shared = alice_priv.exchange(ec.ECDH(), loaded_public_key)
     alice_hkdf = HKDF(algorithm=hashes.SHA256(),length=32,salt=None,info=b'',).derive(alice_shared)
     #Now we can encrypt alice's message and send it over
     print("Sleeping for a bit!")
